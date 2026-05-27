b49a252c8e4346c3f5e19954083319a4a75955a2
fx <fx@fx-world.de>
2026-05-27 21:40:40 +0200

    grouped the search parameters

--- java/de.fxworld.euvdapi.test/src/test/java/de/fxworld/euvdapi/test/ScoresAndFiltersApiTest.java
+++ java/de.fxworld.euvdapi.test/src/test/java/de/fxworld/euvdapi/test/ScoresAndFiltersApiTest.java
@@ -19,10 +19,10 @@ class ScoresAndFiltersApiTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		 ApiClient defaultClient = Configuration.getDefaultApiClient();
-	     defaultClient.setBasePath("https://euvdservices.enisa.europa.eu/api");
-	        
-	     apiInstance = new ScoresAndFiltersApi(defaultClient);
+		ApiClient defaultClient = Configuration.getDefaultApiClient();
+		defaultClient.setBasePath("https://euvdservices.enisa.europa.eu/api");
+
+		apiInstance = new ScoresAndFiltersApi(defaultClient);
 	}
 
 	@AfterEach
@@ -31,60 +31,59 @@ class ScoresAndFiltersApiTest {
 
 	@Test
 	void testLastVulnerabilities() throws ApiException {
-		 VulnerabilityPage result = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-		 assertNotNull(result);
-		 assertNotNull(result.getItems());
-		 assertEquals(10, result.getItems().size());
+		VulnerabilityPage result = apiInstance.searchVulnerabilities().execute();
+		assertNotNull(result);
+		assertNotNull(result.getItems());
+		assertEquals(10, result.getItems().size());
+		
+		VulnerabilityPage scoreResult = apiInstance.searchVulnerabilities().fromScore(BigDecimal.ZERO).toScore(BigDecimal.TEN).execute();
+		assertNotNull(scoreResult);
+		assertNotNull(scoreResult.getItems());
+		assertEquals(10, scoreResult.getItems().size());
 		 
-		 VulnerabilityPage scoreResult = apiInstance.searchVulnerabilities(BigDecimal.ZERO, BigDecimal.TEN, null, null, null, null, null, null, null, null, null, null, null, null, null);
-		 assertNotNull(scoreResult);
-		 assertNotNull(scoreResult.getItems());
-		 assertEquals(10, scoreResult.getItems().size());
+		VulnerabilityPage epssResult = apiInstance.searchVulnerabilities().fromEpss(BigDecimal.ZERO).toEpss(BigDecimal.TEN).execute();
+		assertNotNull(epssResult);
+		assertNotNull(epssResult.getItems());
+		assertEquals(10, epssResult.getItems().size());
 		 
-		 VulnerabilityPage epssResult = apiInstance.searchVulnerabilities(null, null, BigDecimal.ZERO, BigDecimal.TEN, null, null, null, null, null, null, null, null, null, null, null);
-		 assertNotNull(epssResult);
-		 assertNotNull(epssResult.getItems());
-		 assertEquals(10, epssResult.getItems().size());
-		 
-		 String fromDate = LocalDate.of(2024, 1, 1).format(DateTimeFormatter.ISO_LOCAL_DATE);
-		 String toDate = LocalDate.of(2024, 12, 31).format(DateTimeFormatter.ISO_LOCAL_DATE);
-		 VulnerabilityPage dateResult = apiInstance.searchVulnerabilities(null, null, null, null, fromDate, toDate, null, null, null, null, null, null, null, null, null);
-		 assertNotNull(dateResult);
-		 assertNotNull(dateResult.getItems());
-		 assertEquals(10, dateResult.getItems().size());
+		String fromDate = LocalDate.of(2024, 1, 1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+		String toDate = LocalDate.of(2024, 12, 31).format(DateTimeFormatter.ISO_LOCAL_DATE);
+		VulnerabilityPage dateResult = apiInstance.searchVulnerabilities().fromDate(fromDate).toDate(toDate).execute();
+		assertNotNull(dateResult);
+		assertNotNull(dateResult.getItems());
+		assertEquals(10, dateResult.getItems().size());
 
-		 String product = ".NET 8.0";
-		 VulnerabilityPage productResult = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, product, null, null, null, null, null, null);
-		 assertNotNull(productResult);
-		 assertNotNull(productResult.getItems());
-		 assertEquals(10, productResult.getItems().size());
+		String product = ".NET 8.0";
+		VulnerabilityPage productResult = apiInstance.searchVulnerabilities().product(product).execute();
+		assertNotNull(productResult);
+		assertNotNull(productResult.getItems());
+		assertEquals(10, productResult.getItems().size());
 
-		 String vendor = "microsoft";
-		 VulnerabilityPage vendorResult = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, null, vendor, null, null, null, null, null);
-		 assertNotNull(vendorResult);
-		 assertNotNull(vendorResult.getItems());
-		 assertEquals(10, vendorResult.getItems().size());
+		String vendor = "microsoft";
+		VulnerabilityPage vendorResult = apiInstance.searchVulnerabilities().vendor(vendor).execute();
+		assertNotNull(vendorResult);
+		assertNotNull(vendorResult.getItems());
+		assertEquals(10, vendorResult.getItems().size());
 		 
-		 String assigner = "microsoft";
-		 VulnerabilityPage assignerResult = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, null, null, assigner, null, null, null, null);
-		 assertNotNull(assignerResult);
-		 assertNotNull(assignerResult.getItems());
-		 assertEquals(10, assignerResult.getItems().size());
+		String assigner = "microsoft";
+		VulnerabilityPage assignerResult = apiInstance.searchVulnerabilities().assigner(assigner).execute();
+		assertNotNull(assignerResult);
+		assertNotNull(assignerResult.getItems());
+		assertEquals(10, assignerResult.getItems().size());
 		 
-		 VulnerabilityPage exploitedResult = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, null, null, null, Boolean.TRUE, null, null, null);
-		 assertNotNull(exploitedResult);
-		 assertNotNull(exploitedResult.getItems());
-		 assertEquals(10, exploitedResult.getItems().size());
+		VulnerabilityPage exploitedResult = apiInstance.searchVulnerabilities().exploited(Boolean.TRUE).execute();
+		assertNotNull(exploitedResult);
+		assertNotNull(exploitedResult.getItems());
+		assertEquals(10, exploitedResult.getItems().size());
 		 
-		 VulnerabilityPage pageResult = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, null, null, null, null, Integer.valueOf(2), null, null);
-		 assertNotNull(pageResult);
-		 assertNotNull(pageResult.getItems());
-		 assertEquals(10, pageResult.getItems().size());
+		VulnerabilityPage pageResult = apiInstance.searchVulnerabilities().page(Integer.valueOf(2)).execute();
+		assertNotNull(pageResult);
+		assertNotNull(pageResult.getItems());
+		assertEquals(10, pageResult.getItems().size());
 		 
-		 VulnerabilityPage sizeResult = apiInstance.searchVulnerabilities(null, null, null, null, null, null, null, null, null, null, null, null, null, null, Integer.valueOf(100));
-		 assertNotNull(sizeResult);
-		 assertNotNull(sizeResult.getItems());
-		 assertEquals(100, sizeResult.getItems().size());
-
+		VulnerabilityPage sizeResult = apiInstance.searchVulnerabilities().size(Integer.valueOf(100)).execute();
+		assertNotNull(sizeResult);
+		assertNotNull(sizeResult.getItems());
+		assertEquals(100, sizeResult.getItems().size());
 	}
 }
